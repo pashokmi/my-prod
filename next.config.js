f851c0ecb82dc3c372290ddd16369ac83d8840ff
Павел Мельник <pavelmelnik@MacBook-Air-Pavel.local>
2022-01-15 23:19:18 +0200

    added 1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    }
-
-    return config
-  },
-  images: {
-    loader: 'imgix',
-    path: '/'
-  }
+  reactStrictMode: true,
 }
+
+// module.exports = {
+//   webpackDevMiddleware: (config) => {
+//     config.watchOptions = {
+//       poll: 1000,
+//       aggregateTimeout: 300
+//     }
+
+//     return config
+//   },
+//   images: {
+//     loader: 'imgix',
+//     path: '/'
+//   }
+// }
 
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,34 +1,76 @@
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { StyledInput, StyledLabel, StyledButton } from '../ui/styled'
 
 const Body = styled.div`
   background-color: transparent;
-  display:flex;
-  flex-wrap: wrap;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 
   /* box-shadow: 0px 15px 15px white; */
-
+`
+ const StyledInput = styled.input `
+width: 100%;
+border: none;
+background-color: white;
+padding: 15px;
+border-radius: 20px;
+outline: none;
+margin-bottom: 10px;
 `
 
+ const StyledLabel = styled.h1`
+  color: white;
+  font-size:25;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 20px;
+`
+ const StyledButton = styled.button`
+  border-radius: 20px;
+  padding: 15px;
+  background-color: #fff;
+  color: #000;
+  margin: 0auto;
+  cursor: pointer;
+  width: 100%;
+  &:hover{
+    background-color: #abbac3;
+  }
 
+`
 export const Auth = () => {
   const [inputValue, setInputValue] = useState('')
+  const [error, setError] = useState(false)
   const router = useRouter()
-  const buttonHandler = ()=>{
-    router.push('/nav-bar')
+
+  const buttonHandler = () => {
+    if (inputValue===1111) {
+      router.push('/nav-bar')
+    } else {
+      setError
+    }
   }
-  const onChange = e => {
+  const onChange = (e) => {
     setInputValue(e.target.value)
   }
   console.log(inputValue)
   return (
     <Body>
-      <StyledLabel> Hello, enter authorization code !      </StyledLabel>
+      <StyledLabel> Hello, enter authorization code ! </StyledLabel>
 
-        <StyledInput placeholder='Enter code' type={'password'} value={inputValue} onChange={onChange}/>
+      <StyledInput
+        placeholder="Enter code"
+        type={'password'}
+        value={inputValue}
+        onChange={onChange}
+      />
+      <b style={{color:'red', marginBottom:'10px'}}>{setError ? 'Не верный пароль' : 'wern'}</b>
       <StyledButton onClick={buttonHandler}>Enter</StyledButton>
     </Body>
   )
